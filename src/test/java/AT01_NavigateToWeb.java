88a2baf02a9293b6e1d3391e7c4dc88c28732e98
softwaretestlab <35885117+softwaretestlab@users.noreply.github.com>
2023-02-10 13:26:24 +1100

    Update AT01_NavigateToWeb.java

--- src/test/java/AT01_NavigateToWeb.java
+++ src/test/java/AT01_NavigateToWeb.java
@@ -9,6 +9,6 @@ public class AT01_NavigateToWeb {
          System.out.println("This is test four");
         System.out.println("This is test three-3");
         System.out.println("This is test five");
-        System.out.println("This is test five");
+        System.out.println("This is test six");
     }
 }
